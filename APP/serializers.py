543514ea6b86c3c8eb0b9e76c06e2a96c4ac457d
lulanovl <lulanovvvl@gmail.com>
2022-04-07 00:58:08 +0600

    first commit

---
+++ APP/serializers.py
@@ -0,0 +1,7 @@
+
+from rest_framework import serializers
+
+class NewtonSerializer(serializers.Serializer):
+    equation = serializers.CharField(max_length = 500)
+    first = serializers.IntegerField()
+    second = serializers.IntegerField()
